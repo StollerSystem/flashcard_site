9a0c5da933cee68b42371b8ad5737cd5934df9b4
Ben Stoller <stollersystemsound@gmail.com>
2020-08-11 12:01:40 -0700

    start buliding more js
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>
    Co-authored-by: Grant L Eadie <grantleadie@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("li#javascript").click(function() {    
+    $("p").toggle("hidden");
+  });
+});
--- js/stripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
-  });
-});
