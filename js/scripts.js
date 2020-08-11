1e381ba49e90de92ae99b942d54b2b9b4b918312
Ben Stoller <stollersystemsound@gmail.com>
2020-08-11 16:51:24 -0700

    add more js and css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,44 @@
 $(document).ready(function() {
-  $("li#javascript").click(function() {    
-    $("li#javascript p").toggle("hidden");
+  $("li#javascript").click(function() {     
+    $("#javascript p").toggle("hidden");
   });
+
+  $("li#operators").click(function() {     
+    $("#operators p").toggle("hidden");
+  });
+
+  $("li#variables").click(function() {     
+    $("#variables p").toggle("hidden");
+  });
+
+  $("li#variableNaming").click(function() {     
+    $("#variableNaming p").toggle("hidden");
+  });
+
+  $("li#functions").click(function() {     
+    $("#functions p").toggle("hidden");
+  });
+
+  $("li#methods").click(function() {     
+    $("#methods p").toggle("hidden");
+  });
+
+  $("li#arguments").click(function() {     
+    $("#arguments p").toggle("hidden");
+  });
+
+  $("li#parameters").click(function() {     
+    $("#parameters p").toggle("hidden");
+  });
+
+  $("li#return").click(function() {     
+    $("#return p").toggle("hidden");
+  });
+
+
+
+  // $(".list > li > p").click(function() {
+  //   $(this).parent().toggleClass('.hidden');
+  //   return false;
+
 });
